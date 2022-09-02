d0082932e57109c9486e846ff31d90c8ae8c2edb
Stefan <stefan.altheimer@ausy-technologies.de>
2022-09-02 13:18:17 +0200

    Added js create function

---
+++ mjs_fs/api_lmic.js
@@ -0,0 +1,19 @@
+let LORA = {
+
+
+  _create: ffi('void *mgos_LORA_create(int, int, int, int)'),
+  _init: ffi('void *mgos_LORA_init(void *)'),
+
+
+  createLMIC: function(NSSPin, NRESETPin, txEnPin, rxEnPin){
+    let obj = Object.create(LORA._proto);
+    obj.lora = LORA._create();
+    let res = LORA._init(obj.lora);
+    if (res !== true) {
+      return undefined;
+    }
+    return obj;
+
+  },
+
+}
