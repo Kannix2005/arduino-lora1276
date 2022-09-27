460c6a73c37d15c0eab20b755754108cb076c9f0
Stefan <stefan.altheimer@ausy-technologies.de>
2022-09-27 13:59:46 +0200

    Init

--- mjs_fs/api_arduino_lora1276.js
+++ mjs_fs/api_arduino_lora1276.js
@@ -2,10 +2,24 @@ let LORA = {
 
 
   _create: ffi('void *mgos_LORA_create(int, int, int, int)'),
-  _init: ffi('void *mgos_LORA_init(void *)'),
+  _init: ffi('bool *mgos_LORA_init(void*)'),
+  _tx: ffi('bool *mgos_LORA_txPacket(void*, int, int)'),
+  _rx: ffi('bool *mgos_LORA_rxPacket(void*, int)'),
+  _waitIRQ: ffi('bool *mgos_LORA_waitIrq(void*, int)'),
+  _rxInit: ffi('bool *mgos_LORA_rxInit(void*, int, int)'),
+  _enterStdBy: ffi('void *mgos_LORA_enterStbyMode(void*)'),
+  _enterSlp: ffi('void *mgos_LORA_enterSleepMode(void*)'),
+  _clrInt: ffi('void *mgos_LORA_clrInterrupt(void*)'),
+  _setFreq: ffi('bool *mgos_LORA_setFrequency(void*, int)'),
+  _setRFParams: ffi('bool *mgos_LORA_setRFpara(void*, int, int, int, int)'),
+  _setPreamblLen: ffi('bool *mgos_LORA_setPreambleLen(void*, int)'),
+  _setHeaderMode: ffi('bool *mgos_LORA_setHeaderMode(void*, int)'),
+  _setPayloadLen: ffi('bool *mgos_LORA_setPayloadLength(void*, int)'),
+  _setTxPower: ffi('bool *mgos_LORA_setTxPower(void*, int)'),
+  _setRxTimeout: ffi('bool *mgos_LORA_setRxTimeOut(void*, int)'),
+  // _readSSI: ffi('void *readRSSI(void*, int)'),
 
-
-  createLORA: function(NSSPin, NRESETPin, txEnPin, rxEnPin){
+  create: function(NSSPin, NRESETPin, txEnPin, rxEnPin){
     let obj = Object.create(LORA._proto);
     obj.lora = LORA._create(NSSPin, NRESETPin, txEnPin, rxEnPin);
     let res = LORA._init(obj.lora);
@@ -13,7 +27,56 @@ let LORA = {
       return undefined;
     }
     return obj;
+  },
+  txPacket: function(sendBuffer, packetLength){
+    return this._tx(this, sendBuffer, packetLength)
+  },
+  rxPacket: function(receiveBuffer){
+    return this._tx(this, receiveBuffer)
+  },
+  waitIRQ: function(loraObj){
 
   },
+  rxInit: function(loraObj){
+
+  },
+  enterStandby: function(loraObj){
+
+  },
+  enterSleep: function(loraObj){
+
+  },
+  clearInterrupt: function(loraObj){
+
+  },
+  setFrequency: function(loraObj){
+
+  },
+  setRFParameters: function(loraObj){
+
+  },
+  setPreambleLength: function(loraObj){
+
+  },
+  setHeaderMode: function(loraObj){
+
+  },
+  setPayloadLength: function(loraObj){
+
+  },
+  setTXPower: function(loraObj){
+
+  },
+  setRXTimeout: function(loraObj){
+
+  },
+  doReadSSI: function(loraObj){
+
+  },
+  _proto: {
+    info: function (){
+      return "LORA Interface"
+    }
+  }
 
 }
